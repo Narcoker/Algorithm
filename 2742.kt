d2a93330150d14169ef027b767961eb127d363d2
Narcoker <jykim9868@gmail.com>
2021-07-19 13:15:45 +0900

    백준 알고리즘: Solved

---
+++ 2742.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main() {
+    val s = Scanner(System.`in`)
+    val n = s.nextInt()
+
+    for (i in n downTo 1)
+        println(i)
+}
---
+++ 2753.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main(){
+    val s = Scanner(System.`in`)
+    val n = s.nextInt()
+
+    if(n % 400 == 0){
+        println(1)
+    }
+    else if((n%4 == 0 && n % 100 != 0)){
+        println(1)
+    }g
+    else println(0)
+}
